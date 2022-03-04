399ed9a12ba25f86ca184b61d2fce75f4b6bb7df
egg <egg810207@gmail.com>
2022-03-05 03:02:50 +0800

    新增輸入框、按鈕、及聊天室區域高度設定

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="h-100 d-flex justify-content-center">
     <NavBar @after-show-tweet-modal="afterShowTweetModal" />
+
+    <!-- 左區卡片 -->
     <div class="chatroom-users">
-     <ChatroomUserCards />
+      <ChatroomUserCards />
     </div>
+
+    <!-- 右區聊天室 -->
     <div class="chatroom-content">
       <h5 class="chatroom-content-title p-3">公開聊天室</h5>
-      <div class="m-5">未處理 公開聊天 輸入框 內容區 ???????</div>
+      <div class="chatroom-message-container">
+        未處理 公開聊天內容區 ???????
+      </div>
+      <div class="chatroom-panel">
+        <input
+          class="chatroom-input"
+          placeholder="輸入訊息..."
+          type="text"
+        />
+        <button type="submit" class="chatroom-btn">
+          <img class="chatroom-img" src="https://i.imgur.com/1WQnkAO.png" />
+        </button>
+      </div>
     </div>
+
     <TweetModal v-if="showModal" @after-hide-modal="afterHideModal" />
   </div>
 </template>
@@ -47,20 +64,56 @@ export default {
 </script>
 
 <style scoped>
+/* 左區卡片 */
 .chatroom-users {
   min-width: 390px;
   border-left: 1px solid #e6ecf0;
   border-right: 1px solid #e6ecf0;
 }
+
+/* 右區聊天室 */
 .chatroom-content {
   min-width: 600px;
-  height: 74px;
-  border-bottom: 1px solid #e6ecf0;
+  border-right: 1px solid #e6ecf0;
 }
 
 .chatroom-content-title {
+  height: 74px;
   line-height: 37px;
   font-size: 24px;
   font-weight: 700;
+  border-bottom: 1px solid #e6ecf0;
+}
+
+/* 聊天內容區 */
+.chatroom-message-container {
+  height: calc(100vh - 130px);
+}
+
+.chatroom-panel{
+  height: 64px;
+  padding: 10px;
+  display: flex;
+  border-top: 1px solid #e6ecf0;
+  border-bottom: 1px solid #e6ecf0;
+}
+
+.chatroom-input{
+  flex: 1;
+  height: 35px;
+  outline: none;
+  border: none;
+  padding: 10px;
+  background: #e6ecf0;
+  border-radius: 50px;
+  font-size: 18px;
+  font-weight: 500;
+}
+.chatroom-btn {
+  margin-bottom: 10px;
+}
+.chatroom-img {
+  width: 25px;
+  height: 25px;
 }
 </style>
--- src/views/Message.vue
+++ src/views/Message.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="h-100 d-flex justify-content-center">
     <NavBar @after-show-tweet-modal="afterShowTweetModal" />
+
+    <!-- 左區卡片 -->
     <div class="message-users">
       <MessageUserCards />
     </div>
+
+    <!-- 右區聊天室 -->
     <div class="message-content">
+      <div class="message-content-title">
         <p class="message-content-name">name</p>
         <span class="message-content-account">@account</span>
-        <div class="m-5">未處理 個人聊天 輸入框 內容區 ???????</div>
+      </div>
+      <div class="message-container">
+        未處理 私人聊天內容區 ???????
+      </div>
+      <div class="message-panel">
+        <input class="message-input" placeholder="輸入訊息..." type="text" />
+        <button class="message-btn">
+          <img class="message-img" src="https://i.imgur.com/1WQnkAO.png" />
+        </button>
+      </div>
     </div>
+
     <TweetModal v-if="showModal" @after-hide-modal="afterHideModal" />
   </div>
 </template>
@@ -48,22 +63,62 @@ export default {
 </script>
 
 <style scoped>
+/* 左區卡片 */
 .message-users {
   min-width: 390px;
   border-left: 1px solid #e6ecf0;
   border-right: 1px solid #e6ecf0;
 }
+
+/* 右區聊天室 */
 .message-content {
   min-width: 600px;
+  border-right: 1px solid #e6ecf0;
+}
+
+.message-content-title {
   height: 74px;
   border-bottom: 1px solid #e6ecf0;
 }
+
 .message-content-name {
   margin-left: 24px;
   font-size: 24px;
   font-weight: 700;
 }
+
 .message-content-account {
-  margin-left: 24px;
+  margin-left: 20px;
+  color: #6C757D;
+}
+
+/* 聊天內容區 */
+.message-container {
+  height: calc(100vh - 130px);
+}
+.message-panel {
+  height: 64px;
+  padding: 10px;
+  display: flex;
+  border-top: 1px solid #e6ecf0;
+  border-bottom: 1px solid #e6ecf0;
+}
+.message-input {
+  flex: 1;
+  height: 35px;
+  outline: none;
+  border: none;
+  padding: 10px;
+  background: #e6ecf0;
+  border-radius: 50px;
+  font-size: 18px;
+  font-weight: 500;
+}
+.message-btn {
+  margin-bottom: 10px;
+}
+.message-img {
+  width: 25px;
+  height: 25px;
 }
 </style>
