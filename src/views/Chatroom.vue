4541dff1d30e0c5793489f4e0a4215fa3bce5c91
egg <egg810207@gmail.com>
2022-03-06 14:20:29 +0800

    新增讀取歷史訊息

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -46,6 +46,41 @@
               <p class="time">{{ message.time | formatDate }}</p>
             </div>
           </div>
+          <!-- 秀歷史訊息 -->
+          <template v-if="previousMessages.length > 0">
+            <div
+              v-for="message in previousMessages"
+              :key="message.index"
+              :class="{
+                notification: message.typeId === 1,
+                'card-other': message.typeId === 2,
+                'card-self': message.typeId === 3,
+              }"
+            >
+              <!-- v-if判斷相應內容 -->
+              <!-- 1:上線離線通知 -->
+              <span v-if="message.typeId === 1">{{ message.content }}</span>
+              <!-- 2:其他使用者的訊息 -->
+              <div v-if="message.typeId === 2" class="card-other-container">
+                <div class="img-container">
+                  <img :src="message.avatar | emptyImage" alt="avatar" />
+                </div>
+                <div class="content-container">
+                  <div class="text">
+                    {{ message.content }}
+                  </div>
+                  <p class="time">{{ message.time | formatDate }}</p>
+                </div>
+              </div>
+              <!-- 3:自己的訊息 -->
+              <div v-if="message.typeId === 3" class="card-self-container">
+                <div class="text">
+                  {{ message.content }}
+                </div>
+                <p class="time">{{ message.time | formatDate }}</p>
+              </div>
+            </div>
+          </template>
         </div>
         <div class="input-container">
           <input @keyup.enter="sendMessage" v-model="inputMessage" type="text" placeholder="輸入訊息..." />
@@ -67,22 +102,16 @@ import TweetModal from "../components/TweetModal.vue";
 import { mapState } from "vuex";
 import { formatDateFilter } from "./../utils/mixins";
 import { emptyImageFilter } from "../utils/mixins";
+// 取得socketURL 設定在 helpers.js
+import { socketURL } from "../utils/helpers";
 
-// 正式後端伺服器
-const URL1 = "https://twitter-api-chatroom.herokuapp.com/";
-
-// 0305晚上靜易的測試伺服器，有些功能正式版還沒有，但不一定會開
-// const URL1 = "https://dd0f-150-117-29-118.ngrok.io";
-
-// 注意！helpers.js的baseURL也需要切換到同一個伺服器
 // 目前正常顯示上下線通知、歷史聊天紀錄等功能，尚未發布到正式伺服器，因此更新前會收不到
-
 // 取當前使用者token
 const TOKEN = localStorage.getItem("token");
 
 const { io } = require("socket.io-client");
-
-const socket = io(URL1, {
+// 連線建立
+const socket = io(socketURL, {
   auth: { token: TOKEN },
 });
 
@@ -99,6 +128,7 @@ export default {
       messages: [],
       inputMessage: "",
       users: [],
+      previousMessages: []
     };
   },
   mixins: [emptyImageFilter, formatDateFilter],
@@ -111,6 +141,7 @@ export default {
     },
     sendMessage() {
       if (this.inputMessage) {
+        console.log("sendMessage: ", this.inputMessage);
         socket.emit("public message", this.inputMessage);
         this.inputMessage = "";
       }
@@ -118,19 +149,47 @@ export default {
     // 頁面切換到公開聊天室時呼叫(created)
     connectUser() {
       socket.connect();
-      socket.emit("render chatroom", "我進來聊天室了");
+      console.log('切換聊天室')
     },
     // 頁面離開公開聊天室時呼叫(beforeDestroy)
     disconnectUser() {
       socket.disconnect(true);
+      console.log('離開聊天室')
     },
   },
   created() {
+    console.log('previousMessages', this.previousMessages.length)
     // 告知伺服器使用者上線
     this.connectUser();
     // 獲取歷史聊天紀錄（後端只有在被告知使用者上線時，才會送出）
-    socket.on("render chatroom", (data) => {
+    socket.on("render public messages", (data) => {
       console.log("msg清單: ", data);
+
+      for(let i = 0; i < data.length ; i++ ){
+        const msg = data[i];
+        if (msg.senderId === this.currentUser.id) {
+        const thisMessage = {
+          id: -1,
+          content: msg.message,
+          typeId: 3,
+          type: "self",
+          time: msg.createdAt,
+          avatar: "",
+        };
+        this.previousMessages.unshift(thisMessage);
+      } else {
+        const thisMessage = {
+          id: -1,
+          content: msg.message,
+          typeId: 2,
+          type: "other",
+          time: msg.createdAt,
+          avatar: msg.senderAvatar,
+        };
+        this.previousMessages.unshift(thisMessage);
+      }
+    } 
+      
     });
     // 這是接收目前在聊天室的使用者清單
     socket.on("users", (users) => {
@@ -187,7 +246,13 @@ export default {
   },
   beforeDestroy() {
     // 離開頁面時告知後端伺服器
-     this.disconnectUser();
+    this.disconnectUser();
+    // 新增
+    socket.off("render public messages");
+    socket.off("users");
+    socket.off("public message");
+    socket.off("user connect");
+    socket.off("user disconnect");
   },
   computed: {
     ...mapState(["currentUser"]),
